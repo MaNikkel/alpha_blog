541e4fee0cc0efb19bdd246d0dd8bbeb053726a4
Mathias Nikkel <mathiasnikkel@gmail.com>
2022-02-03 13:54:40 -0300

    edit article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -23,4 +23,22 @@ class ArticlesController < ApplicationController
     end
 
   end
+
+  def edit
+    id = params[:id]
+    @article = Article.find(id)
+  end
+
+  def update
+    article_params = params.require(:article).permit(:title, :description)
+    id = params[:id]
+
+    @article = Article.find(id)
+    if @article.update(article_params)
+      flash[:notice] = 'Article updated'
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
 end
