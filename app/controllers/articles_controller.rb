000d6877f069eec24abc4863ab6dc355535c8c05
Mathias Nikkel <mathiasnikkel@gmail.com>
2022-02-03 12:55:45 -0300

    article create

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,16 @@ class ArticlesController < ApplicationController
     id = params[:id]
     @article = Article.find(id)
   end
+
+  def new
+
+  end
+
+  def create
+    @article = Article.new(params.require(:article).permit(:title, :description))
+
+    @article.save
+
+    redirect_to @article
+  end
 end
